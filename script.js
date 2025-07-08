6596275225a5fc9d05e180d640fef9c5e58ed8b8
Arin ♡︎ <collabswitharin@gmail.com>
2025-07-08 11:24:34 +0700

    javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+// toggle menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
